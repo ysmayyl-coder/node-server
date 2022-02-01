1e48f92d3084325a66b50fc184e009b23f378632
ysmayyl <y.saryev@gmail.com>
2022-02-02 04:51:14 +0500

    Made nodejs server running

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log("server on port 3001");
+  });
+});
