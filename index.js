105b7676440e141fc54916632a013ba2adae567e
ysmayyl <y.saryev@gmail.com>
2022-02-03 05:47:54 +0500

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const db = require("./models");
 
+app.use(express.json());
+app.use(cors())
+
+const port = process.env.PORT || 5000;
+
+const posts = require("./routes/Posts");
+app.use("/", posts);
+
 db.sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("http://localhost:3000");
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
   });
 });
